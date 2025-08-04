077f3623658e339bb4f843a383fc563a8f67edb7
Nebiyou-x <Nebiyoutad@gmail.com>
2025-08-04 12:17:57 +0300

    fix routing issues

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ TMDB_API_KEY = config('TMDB_API_KEY')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
