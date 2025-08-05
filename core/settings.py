6ccf8534ed5f7ebf8d0768afc324d2b87e139594
Nebiyou-x <Nebiyoutad@gmail.com>
2025-08-05 08:41:15 +0300

    fix settings

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ TMDB_API_KEY = config('TMDB_API_KEY')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
